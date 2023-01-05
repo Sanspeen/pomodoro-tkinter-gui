3406fb9dd8811a1d4c5f7adfb716667c32d97743
Santiago Franco <ktns0930@gmail.com>
2023-01-04 23:27:12 -0500

    Pomodoro is ready to use :D

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-import time
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,8 +9,18 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-reps = 0
+reps = -1
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+    global reps
+    reps = -1
+    window.after_cancel(timer)
+    lbl_timer.config(text="Timer")
+    canvas.itemconfig(timer_text, text="0:00")
+    btn_start["state"] = "normal"
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
@@ -21,12 +30,12 @@ def start_timer():
     reps += 1
 
     btn_start["state"] = "disabled"
-    time_to_work = 1 * 60
-    short_break = 0.3 * 60
-    long_break = 0.4 * 60
+    time_to_work = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
 
-    if reps % 2 == 0:
-        if reps == 8:
+    if reps % 2 != 0:
+        if reps == 7:
             count_down(long_break)
             lbl_timer.config(text="Long break", fg=RED)
         else:
@@ -40,19 +49,24 @@ def start_timer():
 
 
 def count_down(count):
-    global btn_start
+    global reps
     count_minute = math.floor(count / 60)
     count_seconds = math.floor(count % 60)
+    check_marks = ""
 
     if int(count_seconds) <= 9:
         count_seconds = f"0{count_seconds}"
 
     canvas.itemconfig(timer_text, text=f"{count_minute}:{count_seconds}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
-
+        working_sessions = math.floor(reps / 2)
+        for _ in range(working_sessions):
+            check_marks += "✔"
+        lbl_check.config(text=check_marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -68,13 +82,13 @@ canvas.grid(column=1, row=1)
 lbl_timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 lbl_timer.grid(column=1, row=0)
 
-lbl_check = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 10, "bold"))
+lbl_check = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 10, "bold"))
 lbl_check.grid(column=1, row=3)
 
 btn_start = Button(text="Start", command=start_timer)
 btn_start.grid(column=0, row=2)
 
-btn_reset = Button(text="Reset")
+btn_reset = Button(text="Reset", command=reset_timer)
 btn_reset.grid(column=2, row=2)
 
 window.mainloop()
